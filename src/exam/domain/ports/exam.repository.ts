df59cac6d04c2527c2577ecdaf585070e92a8101
Antonio Morga <anotniomc9719@gmail.com>
2022-11-16 19:17:15 +0100

    Created exam repository and generated exam service and its test

---
+++ src/exam/domain/ports/exam.repository.ts
@@ -0,0 +1,12 @@
+import { iExam } from '../entities/exam.model';
+
+export interface ExamRepository {
+  create({}: iExam): Promise<iExam>;
+  findOne({}: Partial<iExam>): Promise<iExam>;
+  findById(id: string): Promise<iExam>;
+  findByIdAndUpdate(id: string, {}: Partial<iExam>): Promise<iExam>;
+  findByIdAndDelete(id: string): Promise<iExam>;
+  exists(id: string): Promise<boolean>;
+}
+
+export const ExamRepository = Symbol('ExamRepository');
